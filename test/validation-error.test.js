b05ff0619e72be4c28527111bd3b1f7d442b936c
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-07-30 09:53:44 +0700

    validation-error

--- test/validation-error.test.js
+++ test/validation-error.test.js
@@ -4,13 +4,35 @@ describe("Joi", () => {
   it("should return validation error", () => {
     const usernameSchema = Joi.string().min(5).email().required();
 
+    const personSchema = Joi.object({
+      name: Joi.string().required(),
+      age: Joi.number().min(0).max(100),
+      address: Joi.string().required(),
+    });
+
     const result = usernameSchema.validate("user");
+    const resultPerson = personSchema.validate(
+      {
+        name: "Rizki",
+        age: -1,
+        address: 23,
+      },
+      {
+        abortEarly: false,
+      },
+    );
     console.log(result);
 
-    if (result.error) {
-      result.error.details.forEach((detail) => {
-        console.log(`${detail.path} = ${detail.message}`);
+    console.log(resultPerson);
+
+    const errorLog = [];
+
+    if (resultPerson.error) {
+      resultPerson.error.details.forEach((detail) => {
+        errorLog.push({ path: detail.path[0], message: detail.message });
       });
     }
+
+    errorLog.forEach((err) => console.log(err.path));
   });
 });
